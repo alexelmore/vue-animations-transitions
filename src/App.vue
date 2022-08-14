9c95b3220f2567d97cd64cfd15947fb70065a825
aelmore49 <alexelmore49@gmail.com>
2022-08-14 10:22:32 -0500

    add Vue Transition component around elements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="container">
+    <div class="container">
+      <!--
+         The Transition component is a built-in Vue Component that, when applied, adds and removes extra CSS "enter" utility classes to the element that it is wrapping, which allow the dev to animate said element's appearance when it mounts the DOM
+      -->
+      <transition name="text">
+        <p v-show="paragraphVisability">This is only sometime visable</p>
+      </transition>
+      <!-- 
+        Similarly, after the element that is being wrapped by Vue's Transition component un-mounts the DOM, the Transition component adds and removes extra CSS utility "leave" classes to the element that it is wrapping, which allow the dev to animate said element's removal from the DOM
+      -->
+
+      <button @click="toggleParagraph">Toggle Paragraph</button>
+    </div>
     <font-awesome-icon
       :class="{
         animate: animatedBlock,
@@ -9,12 +22,14 @@
       icon="fa-brands fa-vuejs"
     />
 
-    <button @click="doAnimation">Animate</button>
+    <button @click="doAnimation">{{ ctaText }}</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
+
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -26,6 +41,7 @@ export default {
     return {
       dialogIsVisible: false,
       animatedBlock: false,
+      paragraphVisability: false,
     };
   },
   methods: {
@@ -38,10 +54,13 @@ export default {
     doAnimation() {
       this.animatedBlock = !this.animatedBlock;
     },
-    reset() {
-      setTimeout(() => {
-        this.animatedBlock = false;
-      }, 2000);
+    toggleParagraph() {
+      this.paragraphVisability = !this.paragraphVisability;
+    },
+  },
+  computed: {
+    ctaText() {
+      return this.animatedBlock ? "Stop Animation" : "Start Animation";
     },
   },
 };
@@ -87,6 +106,7 @@ button:active {
 }
 .container {
   max-width: 40rem;
+  min-height: 200px;
   margin: 2rem auto;
   display: flex;
   justify-content: center;
@@ -102,6 +122,34 @@ button:active {
   animation: animate 2.5s cubic-bezier(0.37, 0, 0.63, 1) 0s infinite normal forwards;
 }
 
+/* Default CSS utility classes added by Vue's Transition Component when the element that it is wrapping first gets added to the DOM */
+/* .v-enter-from {
+  opacity: 0;
+  transform: rotateX(-100deg);
+} */
+.text-enter-active {
+  animation: textin 0.3s ease-out;
+}
+/* .v-enter-to {
+  opacity: 1;
+  transform: rotateX(0deg);
+} */
+
+/* Default CSS utility classes added by Vue's Transition Component when the element that it is wrapping gets removed from the DOM */
+
+/* .v-leave-from {
+  opacity: 1;
+  transform: rotateX(0deg);
+} */
+.text-leave-active {
+  animation: textout 0.3s ease-out;
+}
+/* .v-leave-to {
+  opacity: 0;
+  transform: rotateX(-100deg);
+} */
+
+/* Animation for Vue logo box */
 @keyframes animate {
   0%,
   100% {
@@ -137,6 +185,36 @@ button:active {
   90% {
     transform: rotate(8deg);
     background-color: rgb(18, 255, 18);
+  }
+}
+
+/* Animation for adding paragraph text to the DOM */
+@keyframes textin {
+  0% {
+    opacity: 0;
+    transform: rotateX(-100deg);
+    transform-origin: top;
+  }
+
+  100% {
+    opacity: 1;
+    transform: rotateX(0deg);
+    transform-origin: top;
+  }
+}
+
+/* Animation for removing paragraph text to the DOM */
+@keyframes textout {
+  0% {
+    opacity: 1;
+    transform: rotateX(0deg);
+    transform-origin: top;
+  }
+
+  100% {
+    opacity: 0;
+    transform: rotateX(-100deg);
+    transform-origin: top;
   }
 }
 </style>
